c9f9edfabae2d9af28e0b457a105fef3083bf42e
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2025-01-28 14:23:29 +0300

    Update NetworkManagerHandler.cs

--- Assets/Reconnect/NetworkManagerHandler.cs
+++ Assets/Reconnect/NetworkManagerHandler.cs
@@ -16,25 +16,11 @@ public class NetworkManagerHandler : MonoBehaviour
     // Когда клиент отключается
     private void HandleClientDisconnected(ulong clientId)
     {
-        GameObject playerObject = FindPlayerObjectByClientId(clientId);  // Найти объект игрока
+        GameObject playerObject = this.gameObject;  // Найти объект игрока
         if (playerObject != null)
            positionManager.SavePlayerPosition(playerObject.transform.position, clientId);  // Сохранение позиции на сервере
 
         
-    }
-
-    private GameObject FindPlayerObjectByClientId(ulong clientId)
-    {
-        foreach (var player in NetworkManager.Singleton.SpawnManager.SpawnedObjects.Values)
-        {
-            // Проверяем, является ли текущий объект объектом игрока
-            if (player.IsOwner && player.OwnerClientId == clientId)
-            {
-                return player.gameObject;
-            }
-        }
-
-        return null;
     }
 
 }
